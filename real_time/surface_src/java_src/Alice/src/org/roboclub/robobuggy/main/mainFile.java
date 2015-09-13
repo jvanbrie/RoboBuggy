cf2e4af4ac6d8444c4211c7451090d9b4e520e9b
Vasu Agrawal <vasu_agrawal@yahoo.com>
2015-09-13 15:56:55 -0400

    Attempting to commit for the second time.

--- real_time/surface_src/java_src/Alice/src/org/roboclub/robobuggy/main/mainFile.java
+++ real_time/surface_src/java_src/Alice/src/org/roboclub/robobuggy/main/mainFile.java
@@ -4,6 +4,7 @@ import org.roboclub.robobuggy.calculatedNodes.CalculatedNodeEnum;
 import org.roboclub.robobuggy.calculatedNodes.SimpleEncoderCalculator;
 import org.roboclub.robobuggy.calculatedNodes.SimpleIMUCalculator;
 import org.roboclub.robobuggy.logging.RobotLogger;
+import org.roboclub.robobuggy.nodes.RealNodeEnum;
 import org.roboclub.robobuggy.ros.Message;
 import org.roboclub.robobuggy.ros.MessageListener;
 import org.roboclub.robobuggy.ros.SensorChannel;
@@ -63,36 +64,36 @@ public static void bringup_sim() throws Exception {
 
 		SensorManager sm = SensorManager.getInstance();
 
-		//		sm.newRealSensor(SensorChannel.IMU, "COM5");
-//		sm.newRealSensor(SensorChannel.GPS, "COM2");
-//		sm.newRealSensor(SensorChannel.ENCODER, "COM42");
-//		sm.newRealSensor(SensorChannel.DRIVE_CTRL, "COM69");
-		String path = "D:\\Vasu\\Documents\\RoboClub\\RoboBuggy\\offline\\offline_tools\\java_src\\FauxArduino\\logs\\2015-04-12-06-22-37\\sensors.txt";
-		String path2 = "D:\\Vasu\\Documents\\RoboClub\\RoboBuggy\\offline\\offline_tools\\java_src\\FauxArduino\\logs\\2015-04-12-06-22-38\\sensors.txt";
-//		String path2 = "C:\\Users\\Vasu\\Documents\\RoboClub\\RoboBuggy\\offline\\offline_tools\\java_src\\FauxArduino\\logs\\2015_slurpee_day\\dualTest.txt";
-		sm.newFauxSensors(path
-			//,SensorChannel.IMU
-			,SensorChannel.ENCODER
-			,SensorChannel.GPS
-			//,SensorChannel.DRIVE_CTRL
-			);
-		sm.newFauxSensors(path2
-			,SensorChannel.IMU
-			,SensorChannel.DRIVE_CTRL
-			);
-		
-		//This will *effectively* disable it, but it seems that because it's subscribing to things
-		//there are still parts of the GUI which think a sensor exists. This may or may not be an issue?
-		//ISSUE: Seems like there's a delay in enabling / disabling it?
-		//SOLUTION: It looks like the delay was caused by the call of newCalculatedSensor, which isn't instantaneous
-		//		this may or may not be a problem? Will ask Trevor.
-		sm.disableFauxNode(path, SensorChannel.ENCODER);
-		sm.disableFauxNode(path2, SensorChannel.IMU);
-		
-		//I've just discovered that sending values from different sensors doesn't break the entire system, but instead 
-		//just causes a lot of jittery / jumpy behavior.
-		sm.newCalculatedSensor(CalculatedNodeEnum.ENCODER, SensorChannel.ENCODER, new SimpleEncoderCalculator(), 100);
-		sm.newCalculatedSensor(CalculatedNodeEnum.IMU, SensorChannel.IMU, new SimpleIMUCalculator(), 100);
+		sm.newRealSensor(RealNodeEnum.IMU, SensorChannel.IMU, "COM4");
+		sm.newRealSensor(RealNodeEnum.GPS, SensorChannel.GPS, "COM7");
+		sm.newRealSensor(RealNodeEnum.ENCODER, SensorChannel.ENCODER, "COM14");
+	
+//      The below is left as usage example for the simulation and calculation nodes.
+//		String path = "D:\\Vasu\\Documents\\RoboClub\\RoboBuggy\\offline\\offline_tools\\java_src\\FauxArduino\\logs\\2015-04-12-06-22-37\\sensors.txt";
+//		String path2 = "D:\\Vasu\\Documents\\RoboClub\\RoboBuggy\\offline\\offline_tools\\java_src\\FauxArduino\\logs\\2015-04-12-06-22-38\\sensors.txt";
+//		sm.newFauxSensors(path
+//			//,SensorChannel.IMU
+//			,SensorChannel.ENCODER
+//			,SensorChannel.GPS
+//			//,SensorChannel.DRIVE_CTRL
+//			);
+//		sm.newFauxSensors(path2
+//			,SensorChannel.IMU
+//			,SensorChannel.DRIVE_CTRL
+//			);
+//		
+//		//This will *effectively* disable it, but it seems that because it's subscribing to things
+//		//there are still parts of the GUI which think a sensor exists. This may or may not be an issue?
+//		//ISSUE: Seems like there's a delay in enabling / disabling it?
+//		//SOLUTION: It looks like the delay was caused by the call of newCalculatedSensor, which isn't instantaneous
+//		//		this may or may not be a problem? Will ask Trevor.
+//		sm.disableFauxNode(path, SensorChannel.ENCODER);
+//		sm.disableFauxNode(path2, SensorChannel.IMU);
+//		
+//		//I've just discovered that sending values from different sensors doesn't break the entire system, but instead 
+//		//just causes a lot of jittery / jumpy behavior.
+//		sm.newCalculatedSensor(CalculatedNodeEnum.ENCODER, SensorChannel.ENCODER, new SimpleEncoderCalculator(), 100);
+//		sm.newCalculatedSensor(CalculatedNodeEnum.IMU, SensorChannel.IMU, new SimpleIMUCalculator(), 100);
 		
 		new Subscriber(SensorChannel.ENCODER.getMsgPath(), new MessageListener() {
 			@Override
